bd98fe245d413c59641fc40582f2f81faae327b7
Tetiana Karasova <tkarasova@google.com>
2021-10-12 15:13:38 +0000

    make program runnable with argument

--- grs-dotnet/grs-search/Program.cs
+++ grs-dotnet/grs-search/Program.cs
@@ -6,30 +6,58 @@ namespace grs_search
     {
         static void Main(string[] args)
         {
-            // if (args.Length == 0)
-            // {
-            //     Console.Write("Please pass one of the file names to run");
-            // }
-            // if (args[0] == "SearchSimpleQuery")
-            // {
-            //     SearchSimpleQuery.Search();
-            // }
-            // if (args[0] == "SearchWithFiltering")
-            // {
-            //     SearchWithFiltering.Search();
-            // }
-            SearchSimpleQuery.Search();
-           /* SearchWithBoostSpec.Search();
-            SearchWithFiltering.Search();
-            SearchWithNumericalFacet.Search();
-            SearchWithOrdering.Search();
-            SearchWithPaginationNextPageToken.Search();
-            SearchWithPaginationPageSize.Search();
-            SearchWithQueryExpansion.Search();
-            SearchWithTextualFacetExcludingFilterKey.Search();
-            SearchWithTextualFacet.Search();
-            SearchWithTextualFacetRestrictingValues.Search();
-            SearchWithVariantRollupKeys.Search();*/
+            if (args.Length == 0)
+            {
+                Console.Write("Please run the application again and pass one of the file names to run. ");
+            }
+            if (args[0] == "SearchSimpleQuery")
+            {
+                SearchSimpleQuery.Search();
+            }
+            if (args[0] == "SearchWithBoostSpec")
+            {
+                SearchWithBoostSpec.Search();
+            }
+            if (args[0] == "SearchWithFiltering")
+            {
+                SearchWithFiltering.Search();
+            }
+            if (args[0] == "SearchWithNumericalFacet")
+            {
+                SearchWithNumericalFacet.Search();
+            }
+            if (args[0] == "SearchWithOrdering")
+            {
+                SearchWithOrdering.Search();
+            }
+            if (args[0] == "SearchWithPaginationNextPageToken")
+            {
+                SearchWithPaginationNextPageToken.Search();
+            }
+            if (args[0] == "SearchWithPaginationPageSize")
+            {
+                SearchWithPaginationPageSize.Search();
+            }
+            if (args[0] == "SearchWithQueryExpansion")
+            {
+                SearchWithQueryExpansion.Search();
+            }
+            if (args[0] == "SearchWithTextualFacetExcludingFilterKey")
+            {
+                SearchWithTextualFacetExcludingFilterKey.Search();
+            }
+            if (args[0] == "SearchWithTextualFacet")
+            {
+                SearchWithTextualFacet.Search();
+            }
+            if (args[0] == "SearchWithTextualFacetRestrictingValues")
+            {
+                SearchWithTextualFacetRestrictingValues.Search();
+            }
+            if (args[0] == "SearchWithVariantRollupKeys")
+            {
+                SearchWithVariantRollupKeys.Search();
+            }
         }
     }
 }
